dd0f24e544798b8f7902f63eda55efc351f9127c
AmolDhait <amol.dhait97@gmail.com>
2022-08-24 09:15:15 +0530

    Added Remaining properties to payroll Model

--- main/java/com/bridgelabz/employeepayrollapp/entity/EmployeePayrollData.java
+++ main/java/com/bridgelabz/employeepayrollapp/entity/EmployeePayrollData.java
@@ -1,29 +1,32 @@
 package com.bridgelabz.employeepayrollapp.entity;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.persistence.Entity;
+import lombok.Data;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.List;
 
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@Table
-public class EmployeePayrollData {
+
+public @Data class EmployeePayrollData {
     @Id
     private int employeeId;
     private String name;
     private long salary;
+    private String gender;
+    private String startDate;
+    private String note;
+    private String profilePic;
+    private List<String> department;
 
-    public EmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO) {
+    public EmployeePayrollData(int empId,
+                               EmployeePayrollDTO employeePayrollDTO) {
         this.employeeId = empId;
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
+        this.gender = employeePayrollDTO.gender;
+        this.note = employeePayrollDTO.note;
+        this.startDate = employeePayrollDTO.startDate;
+        this.profilePic = employeePayrollDTO.profilePic;
+        this.department = employeePayrollDTO.department;
     }
 
     //public EmployeePayrollData(){}
